d5c5f7b4f494127b42a8e518513c516b311b61b5
mauriciogaraco <134785678+mauriciogaraco@users.noreply.github.com>
2025-06-02 22:16:58 -0500

    Update Cart.tsx

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -1,2 +1,38 @@
-const Cart = () => <h2 className="p-6">Carrito</h2>;
+import { useNavigate } from "react-router-dom";
+
+import { FaChevronRight } from "react-icons/fa";
+
+import { RootState } from "@/store/store";
+
+import ItemCards from "@/components/atoms/cart/ItemCard";
+import { useSelector } from "react-redux";
+
+const Cart = () => {
+  const orderState = useSelector((state: RootState) => state.orders);
+  const navigate = useNavigate();
+  // Incrementa la cantidad del producto
+
+  console.log(orderState);
+
+  return (
+    <div className="mt-16 pb-20 ">
+      <div className="flex justify-center mt-24 items-center font-ArialBold text-sm  md:text-xl text-grape-900 space-x-4">
+        <a>Shopping Cart</a>
+        <FaChevronRight className="text-gray-500" />
+        <button
+          className="text-gray-500 hover:text-grape-900"
+          onClick={() => navigate("/checkout")}
+        >
+          Checkout
+        </button>
+        <FaChevronRight className="text-gray-500" />
+        <button className="text-gray-500">Order Complete</button>
+      </div>
+      <div className="section-padding pb-16">
+        <ItemCards />
+      </div>
+    </div>
+  );
+};
+
 export default Cart;
